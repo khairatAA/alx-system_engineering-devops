cd342b6a9cdbc08df3600172b55da81e2b851fee
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-29 16:58:48 +0100

    fix: rewrote the codes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,50 +1,57 @@
 #  Install Nginx web server (w/ Puppet)
 
 exec { 'apt_update':
-  command  => 'apt update',
+  command  => '/usr/bin/apt update',
 }
 
-package { 'nginx':
-  ensure   => installed,
-  provider => 'apt',
-  require  => Exec['apt_update'],
+exec { 'nginx_install':
+  command => '/usr/bin/apt install nginx -y',
+  require => Exec['apt_update'],
 }
 
-$file_path = '/etc/nginx/sites-available/default'
+#$file_path = '/etc/nginx/sites-available/default'
 
-$file_content = file($file_path)
+#$file_content = file($file_path)
 
-$replace_content = chomp($file_content).gsub(/listen [::]:80 default_server;/, 'listen 80;')
+# $replace_content = $file_content.strip.gsub(/listen [::]:80 default_server;/, 'listen 80;')
 
-file { $file_path:
-  ensure  => file,
-  content => $replace_content,
-}
+#file { $file_path:
+#  ensure  => file,
+#  content => $replace_content,
+#}
 
 file { '/var/www/html/index.html':
   ensure  => file,
   content => 'Hello World!',
+  require => Exec['nginx_install']
 }
 
-$redirection = "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
+$redirection = "
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
 
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-}"
+        location / {
+                try_files $$uri $$uri/ =404;
+        }
+
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+}
+"
 
 file { '/etc/nginx/sites-available/default':
-  ensure  => file,
+  ensure  => present,
   content => $redirection,
 }
 
-exce { 'nginx_restart':
-  command => 'service nginx restart',
-  require => [File[$file_path], File['/var/www/html/index.html'], File['/etc/nginx/sites-available/default']],
+exec { 'service_restart':
+  command => '/usr/sbin/service nginx restart',
+  require => File['/etc/nginx/sites-available/default'],
 }
