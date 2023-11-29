2fd956e31db1aa4f42b1bc0f9d89a6e64703526b
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-29 17:01:40 +0100

    fix: rewrote the codes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -36,7 +36,7 @@ server {
         server_name _;
 
         location / {
-                try_files $$uri $$uri/ =404;
+                try_files $${uri} $${uri}/ =404;
         }
 
         location /redirect_me {
