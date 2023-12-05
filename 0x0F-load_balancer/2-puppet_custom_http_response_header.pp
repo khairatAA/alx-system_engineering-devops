b474a6bbfb68d3fb5c445df9d40b463fb783408c
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-05 23:37:19 +0100

    add: restructed codes

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,35 +11,11 @@ package { 'nginx':
   ensure     => 'installed',
 }
 
-# custom header
-$custom_header = "
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-	location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files \$uri \$uri/ =404;
-		add_header X-Served-By \$hostname;
-        }
-}
-"
-
 # Handle custom_header
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  content => $custom_header,
-}
-
-# restart nginx
-exec { 'service_restart':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
+file_line { 'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-enabled/default',
+  line   => 'add_header X-Served-By $hostname;',
 }
 
 # start nginx
