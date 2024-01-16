1b79f5072d845abe097480dacb1ac36d6ce974a8
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-16 16:17:41 +0100

    add: returns the number of subscribers from reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""0-subs module"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    number_of_subscribers: returns the number of subscribers
+    (not active users, total subscribers)
+    for a given subreddit
+    """
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        data = r.json()
+
+        subcribers_count = data['data']['subscribers']
+        return subcribers_count
+
+    return 0
