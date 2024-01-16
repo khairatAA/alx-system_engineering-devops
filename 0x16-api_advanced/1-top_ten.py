f490583ece2d778feda87ba3774d11aeb997ba51
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-16 21:11:33 +0100

    add: Top Ten post on reddit api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""1-top-ten module"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    top_ten: prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    Args:
+        subreddit: passed to the url
+    """
+    headers = {'User-Agent': 'My User Agent'}
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+
+    params = {
+            "limit": 10,
+            }
+
+    r = requests.get(url, headers=headers, params=params)
+
+    if r.status_code == 200:
+        data = r.json()
+        results = data.get("data")
+
+        hot_titles = []
+
+        for i in results.get("children"):
+            hot_titles = i.get('data').get('title')
+            print(hot_titles)
+    else:
+        print(None)
+
+    return
