b9fb0cb08bb7dfca24a629f1b424463d7344e30f
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-17 15:29:03 +0100

    fix: fixed comments

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
--- Using strace, find out why Apache is returning a 500 error.
+# Using strace, find out why Apache is returning a 500 error.
 
 exec {'modification':
   command => '/bin/sed -i "s/pphp/php/g" /var/www/html/wp-settings.php',
