c92255b2458b500a0186e525c67ec01760c7103b
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-17 11:42:57 +0100

    add: solution to 500 server error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+-- Using strace, find out why Apache is returning a 500 error.
+
+exec {'modification':
+  command => '/bin/sed -i "s/pphp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/usr/bin', '/usr/sbin']
+}
