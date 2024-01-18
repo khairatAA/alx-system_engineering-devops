ead66b69977682451294984019f27427f4b2a20d
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-18 07:26:04 +0100

    fix: fixed path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error.
 
 exec {'modification':
-  command => '/bin/sed -i "s/pphp/php/g" /var/www/html/wp-settings.php',
-  path    => ['/usr/bin', '/usr/sbin']
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/sbin']
 }
